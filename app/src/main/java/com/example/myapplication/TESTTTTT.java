381b64d2e4f61d0aed5bae602c0200f9fc0a98ea
soodthin <thinhthai963@gmail.com>
2024-11-22 21:38:04 +0700

    test push part 2

--- app/src/main/java/com/example/myapplication/TESTTTTT.java
+++ app/src/main/java/com/example/myapplication/TESTTTTT.java
@@ -1,4 +1,8 @@
 package src.main.java.com.example.myapplication;
 
 public class TESTTTTT {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
